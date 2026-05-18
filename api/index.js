d201dd6811249bfbbff80fd17e649f98f3cfa9f6
msirazu <thebdrazu@gmail.com>
2026-05-18 16:51:28 +0600

    dynamic routing added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('../db');
+const { ObjectId } = require('mongodb');
 const app = express();
 
 app.use(cors());
@@ -13,9 +14,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/pets', async(req, res) => {
-    const db = await connectDB();
-    const data = await db.collection('pets').find().toArray();
-    res.json(data);
+    try {
+        const db = await connectDB();
+        const pets = await db.collection('pets').find().toArray();
+        res.status(200).json({success: true, count: pets.length, data: pets});
+    } catch(error) {
+        res.status(500).json({success: false, message: error.message})
+    }
 })
+
+app.get('/pet-detail/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const db = await connectDB();
+        const query = {
+            _id: new ObjectId(id)
+        }
+        const pet = await db.collection('pets').findOne(query);
+        res.status(200).json({success: true, data: pet});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message})
+    }
+});
 
 module.exports = app;
